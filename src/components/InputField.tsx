c648f6b569e21ba48538fb1a7cc2472dbb24b38f
Frédéric Jacques <gary.bob88@gmail.com>
2022-08-25 14:51:12 -0400

    first usage of yup to display error message

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,17 +1,18 @@
 export interface InputFieldProps {
     label: string
     value: string
+    name: string
     onChange?: (e: any) => void
 }
 
-export const InputField = ({ label, value, onChange }: InputFieldProps) => {
+export const InputField = ({ label, value, name, onChange }: InputFieldProps) => {
     return (
         <>
             <div className="form-field-name">
                 <span>{label}:</span>
             </div>
             <div className="form-field">
-                <input type="text" value={value} onChange={onChange} />
+                <input type="text" name={name} value={value} onChange={onChange} />
             </div>
         </>
     )
--- src/features/ProduitForm.tsx
+++ src/features/ProduitForm.tsx
@@ -3,14 +3,24 @@ import { InputField } from "../components/InputField";
 import { Product } from "../models/Product";
 import { useCreateProductMutation, useUpdateProductMutation } from "../services/ProductsApi";
 import { selectProductSlice, setDescription, setEditProduct, setFormVisible, setPrice, setTitle } from "../services/ProductSlice";
+import * as yup from 'yup'
+import { useState } from "react";
 
 export const ProduitForm = () => {
     const state = useAppSelector(selectProductSlice)
     const dispatch = useAppDispatch()
-    const [postProduct, postResult] = useCreateProductMutation()
-    const [updateProduct, updateResult] = useUpdateProductMutation()
+    const [postProduct] = useCreateProductMutation()
+    const [updateProduct] = useUpdateProductMutation()
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const validation = yup.object({
+        title: yup.string().required().max(255),
+        description: yup.string().required().max(2000),
+        price: yup.number().required().min(0)
+    })
 
     const cleanInfo = () => {
+        setErrorMessage('')
         dispatch(setEditProduct({
             title: "",
             description: "",
@@ -20,21 +30,29 @@ export const ProduitForm = () => {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault()
-        let product: Product = {
-            id: state.editProduct?.id,
-            title: state.editProduct?.title,
-            description: state.editProduct?.description,
-            price: parseInt(state.editProduct?.price ?? "")
+        const isValid = await validation.isValid(state.editProduct)
+        if (isValid) {
+            setErrorMessage('')
+            let product: Product = {
+                id: state.editProduct?.id,
+                title: state.editProduct?.title,
+                description: state.editProduct?.description,
+                price: parseInt(state.editProduct?.price ?? "")
+            }
+            if (product.id == null) {
+                await postProduct(product).unwrap()
+                cleanInfo()
+            }
+            else {
+                await updateProduct(product).unwrap()
+                cleanInfo()
+                dispatch(setFormVisible(false))
+            }
         }
-        console.log(product)
-        if (product.id == null) {
-            await postProduct(product).unwrap()
-            cleanInfo()
-        } 
         else {
-            await updateProduct(product).unwrap()
-            cleanInfo()
-            dispatch(setFormVisible(false))
+            await validation.validate(state.editProduct).catch(err => {
+                setErrorMessage(err.message)
+            })
         }
     }
 
@@ -60,9 +78,24 @@ export const ProduitForm = () => {
             <form onSubmit={handleSubmit}>
                 {state.formVisible ? (
                     <div className="form-field-container">
-                        <InputField label="Titre"       value={state.editProduct?.title ?? ""}             onChange={(e) => dispatch(setTitle(e.target.value))} />
-                        <InputField label="Description" value={state.editProduct?.description ?? ""}       onChange={(e) => dispatch(setDescription(e.target.value))} />
-                        <InputField label="Prix"        value={state.editProduct?.price?.toString() ?? ""} onChange={(e) => dispatch(setPrice(e.target.value))} />
+                        <div className="form-error">
+                            <span>{errorMessage}</span>
+                        </div>
+                        <InputField
+                            label="Titre"
+                            value={state.editProduct?.title ?? ""}
+                            name="title"
+                            onChange={(e) => dispatch(setTitle(e.target.value))} />
+                        <InputField
+                            label="Description"
+                            value={state.editProduct?.description ?? ""}
+                            name="description"
+                            onChange={(e) => dispatch(setDescription(e.target.value))} />
+                        <InputField
+                            label="Prix"
+                            value={state.editProduct?.price?.toString() ?? ""}
+                            name="price"
+                            onChange={(e) => dispatch(setPrice(e.target.value))} />
                         <div className="form-field">
                             <button type="submit">{state.productFormButtonText}</button>
                         </div>
